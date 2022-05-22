2ec9b25a75ebe78bbbccccca803b5d03e00caecb
Mateja <maxon4002@gmail.com>
2022-05-22 18:18:04 +0200

    Netlify Update

--- src/Firebase/firebaseConfig.js
+++ src/Firebase/firebaseConfig.js
@@ -8,13 +8,14 @@ import { signInWithPopup, GoogleAuthProvider, getAuth } from 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAk2nCMXnE2lvJ4W8NazTALwSqHckfsCQQ",
-    authDomain: "imago-de8a8.firebaseapp.com",
-    projectId: "imago-de8a8",
-    storageBucket: "imago-de8a8.appspot.com",
-    messagingSenderId: "271608673039",
-    appId: "1:271608673039:web:44c1dfade83fa61ed3447a",
-    measurementId: "G-7Q5CP7Y8ES"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    // databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
